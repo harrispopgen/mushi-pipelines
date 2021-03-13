b81c6332d815e3ff593f673381b6472d33980958
wsdewitt <wsdewitt@uw.edu>
2021-03-12 16:30:29 -0800

    minor plot tuning

--- 1KG/run1KG.sh
+++ 1KG/run1KG.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #$ -cwd
 
-nextflow run 1KG.nf -with-report report.html -with-timeline timeline.html -with-dag dag.html
-# -resume
+nextflow run 1KG.nf -with-report report.html -with-timeline timeline.html -with-dag dag.html -resume
